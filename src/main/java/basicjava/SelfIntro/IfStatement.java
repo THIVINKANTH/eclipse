ed5b6c1148a62721ab3be8aa5f4c4a53ddfced79
THIVINKANTH <thivinkanth@gmail.com>
2022-09-20 10:26:10 +0530

    maven

---
+++ src/main/java/basicjava/SelfIntro/IfStatement.java
@@ -0,0 +1,19 @@
+package basicjava.SelfIntro;
+
+public class IfStatement {
+
+	public static void main(String[] args) {
+		double a=12.3;
+		double b=34.5;
+		if(a<b)
+		{
+			System.out.println("b is big");
+		}
+		else
+		{
+			System.out.println("a is small");
+		}
+
+	}
+
+}
